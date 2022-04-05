1ff96fc6959e7f343d6b9be3dcf3c9091108d0f8
Rajesh8787 <52686117+Rajesh8787@users.noreply.github.com>
2022-04-05 09:10:45 +0530

    Update Q13.java

--- Q13.java
+++ Q13.java
@@ -9,25 +9,24 @@ public class Q13 {
 		// TODO Auto-generated method stub
 
 		
-		 int n, sum=0, firstDigit, lastDigit;
-	    System.out.println("Enter a 5-digit number\n");
+		 int n, first, second, third, forth, fifth;
+    
+    n=12345;
 
-	    Scanner sc=new Scanner(System.in);
-	    n=sc.nextInt();
-	    
-	   
-	   
-	   
-	    lastDigit = n % 10;
-	  
-	    while(n >= 10)
-	    {
-	        n = n / 10;
-	    }
-	    firstDigit = n;
-	   
-	    sum = firstDigit + lastDigit;
-	    System.out.println("Sum of first and last digit = %d"+ sum);
+    first = n/10000;
+    n %= 10000;
+    
+    second = n/1000; 
+    n %= 1000;
+    
+    third = n/100;
+    n = n%100;
+    
+    forth = n/10;
+    fifth = n/10;
+
+    int sum = first + forth;
+    System.out.println("sum: " + sum);
 	}
 
 } 
