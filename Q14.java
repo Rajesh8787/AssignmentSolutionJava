7c3c3d4614d4c2290f9378a0c121749c80a9bb27
Rajesh8787 <52686117+Rajesh8787@users.noreply.github.com>
2022-04-05 09:09:29 +0530

    Update Q14.java

--- Q14.java
+++ Q14.java
@@ -4,6 +4,24 @@ public class Q14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		 int n = 5696;                                            // Ans : 7818
+
+        int first  = n/1000+2;                                   //first = 7
+ 
+        n = n%1000;                                              // n = 696
+        int second = n/100+2;                                    // second =  8 
+        
+        n = n%100;                                               // n =  96
+        int third = n/10+2;                                      // third = 11
+        third = third/10;                                        // third = 1
+        
+        n = n%10;                                                // n = 6      
+        int fourth = n+2;                                        // fourth = 8
+        
+        String result = Integer.toString(first)+Integer.toString(second)+Integer.toString(third)+Integer.toString(fourth);
+                
+        System.out.println(result);
 
 	}
 
